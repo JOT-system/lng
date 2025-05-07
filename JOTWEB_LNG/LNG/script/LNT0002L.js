822eb5ef2f811451ad99ad1308a27dcab96105ad
慎也 大浜 <sohama@jot.co.jp>
2025-05-07 16:06:12 +0900

    20250507_輸送費明細出力状況画面

--- JOTWEB_LNG/LNG/script/LNT0002L.js
+++ JOTWEB_LNG/LNG/script/LNT0002L.js
@@ -13,10 +13,8 @@ window.onload = function () {
     //表示判断キー
     const VisibleKeyOrgCode = document.getElementById('VisibleKeyOrgCode').value;
 
-    //情シス、高圧ガス以外の場合
-    if (VisibleKeyOrgCode == "") {
-        document.getElementById('WF_ButtonHISTORY').style.display = "none";
-    }
+    //請求調整列編集
+    f_InvoiceCtrlCol();
 }
 
 /**
@@ -157,4 +155,83 @@ function BtnHistOutputClick(obj, lineCnt, fieldNM) {
     document.getElementById("WF_SelectedIndex").value = lineCnt
     document.getElementById("WF_ButtonClick").value = "WF_ButtonHistOutClick";
     document.forms[0].submit();
+}
+
+// ○請求調整列編集
+function f_InvoiceCtrlCol() {
+    var Col = {
+        SPRATE: 2 //特別料金列
+        , TANKA: 3 //単価調整列
+        , KOTEIHI: 4 //固定費調整列
+        , SURCHARGE: 5 //サーチャージ列
+    };
+
+    //**********ヘッダ********************/
+    //1つの列以外を非表示にして表示中の列幅を伸ばす
+    var objTableHR = document.getElementById("pnlListArea_HR").children[0];
+
+    objTableHR.rows[0].cells[Col.TANKA].style.display = "none";
+    objTableHR.rows[0].cells[Col.KOTEIHI].style.display = "none";
+    objTableHR.rows[0].cells[Col.SURCHARGE].style.display = "none";
+
+    var objNewWidth = Number(objTableHR.rows[0].cells[Col.SPRATE].style.width.replace("px","")) +
+        Number(objTableHR.rows[0].cells[Col.TANKA].style.width.replace("px", "")) +
+        Number(objTableHR.rows[0].cells[Col.KOTEIHI].style.width.replace("px", "")) +
+        Number(objTableHR.rows[0].cells[Col.SURCHARGE].style.width.replace("px", "")) + "px";
+
+    objTableHR.rows[0].cells[Col.SPRATE].style.width = objNewWidth
+   
+    //表示名称
+    objTableHR.rows[0].cells[Col.SPRATE].innerHTML = "　請求調整";
+    //ID追加
+    objTableHR.rows[0].cells[Col.SPRATE].id = "seikyuchosei";
+    //タイトル(吹き出し)
+    objTableHR.rows[0].cells[Col.SPRATE].title = "入力が行われている場合、アイコンの色が変わります。\n" +
+        "[凡例]\n" +
+        "特・・・特別料金\n" +
+        "単・・・単価調整\n" +
+        "固・・・固定費調整\n" +
+        "サ・・・サーチャージ"; 
+
+    //**********データ行********************/
+    var objTableDR = document.getElementById("pnlListArea_DR").children[0];
+    for (var i = 0; i < objTableDR.rows.length; i++) {
+
+        //特別料金
+        if (objTableDR.rows[i].cells[Col.SPRATE].innerHTML != "") {
+            objTableDR.rows[i].cells[Col.SPRATE].style.backgroundColor = "#44B3E1";
+            objTableDR.rows[i].cells[Col.SPRATE].style.color = "#FFFFFF";
+        } 
+
+        //単価調整
+        if (objTableDR.rows[i].cells[Col.TANKA].innerHTML != "") {
+            objTableDR.rows[i].cells[Col.TANKA].style.backgroundColor = "#44B3E1";
+            objTableDR.rows[i].cells[Col.TANKA].style.color = "#FFFFFF";
+        } 
+
+        //固定費調整
+        if (objTableDR.rows[i].cells[Col.KOTEIHI].innerHTML != "") {
+            objTableDR.rows[i].cells[Col.KOTEIHI].style.backgroundColor = "#44B3E1";
+            objTableDR.rows[i].cells[Col.KOTEIHI].style.color = "#FFFFFF";
+        } 
+
+        //サーチャージ
+        if (objTableDR.rows[i].cells[Col.SURCHARGE].innerHTML != "") {
+            objTableDR.rows[i].cells[Col.SURCHARGE].style.backgroundColor = "#44B3E1";
+            objTableDR.rows[i].cells[Col.SURCHARGE].style.color = "#FFFFFF";
+        } 
+
+        objTableDR.rows[i].cells[Col.SPRATE].innerHTML = "特";
+        objTableDR.rows[i].cells[Col.SPRATE].title = "";
+
+        objTableDR.rows[i].cells[Col.TANKA].innerHTML = "単";
+        objTableDR.rows[i].cells[Col.TANKA].title = "";
+
+        objTableDR.rows[i].cells[Col.KOTEIHI].innerHTML = "固";
+        objTableDR.rows[i].cells[Col.KOTEIHI].title = "";
+
+        objTableDR.rows[i].cells[Col.SURCHARGE].innerHTML = "サ";
+        objTableDR.rows[i].cells[Col.SURCHARGE].title = "";
+
+    }
 }
